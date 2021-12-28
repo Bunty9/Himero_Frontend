bcb3270b0985cdbfe3fde96b5bb659bef74c035d
Bunty9 <cbipin2000@gmail.com>
2021-12-28 13:49:30 +0530

    latest

--- src/services/admin.service.js
+++ src/services/admin.service.js
@@ -5,34 +5,34 @@ const API_URL = "http://localhost:6969/api/";
 const userid = JSON.parse(localStorage.getItem("user"))?.id;
 
 const instance = axios.create({
-    baseURL: API_URL,
-    timeout: 1000,
-    headers: authHeader(),
+	baseURL: API_URL,
+	timeout: 1000,
+	headers: authHeader(),
 });
 
 const newHome = (homeName) => {
-    return instance.post(API_URL + "newhome", { userid, homeName });
+	return instance.post(API_URL + "newhome", { userid, homeName });
 };
 const newRoom = (homeName, roomName) => {
-    return instance.post(API_URL + "newroom", { userid, homeName, roomName });
+	return instance.post(API_URL + "newroom", { userid, homeName, roomName });
 };
 const newDevice = (homeName, roomName, deviceId, deviceName) => {
-    return instance.post(API_URL + "newdevice", {
-        userid,
-        homeName,
-        roomName,
-        deviceId,
-        deviceName,
-    });
+	return instance.post(API_URL + "newdevice", {
+		userid,
+		homeName,
+		roomName,
+		deviceId,
+		deviceName,
+	});
 };
 
 const turnon = () => {
-    return instance.post(API_URL + "turnon", { userid });
+	return instance.post(API_URL + "turnon", { userid });
 };
 
 export default {
-    newHome,
-    newRoom,
-    newDevice,
-    turnon,
+	newHome,
+	newRoom,
+	newDevice,
+	turnon,
 };
